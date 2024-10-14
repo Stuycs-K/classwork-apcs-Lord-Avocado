419d15b4da63dacc5fa376af607831fd78889092
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-14 19:40:17 -0400

    Wrote and tested concatArray(int[], int[]) function, wrote comments describing functions

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -6,11 +6,21 @@ public class MyArrays {
     int[] a2 = new int[] {};
     System.out.println(arrayToString(a1));
     System.out.println(arrayToString(a2));
+
+    // testing returnCopy
     int[] b1 = returnCopy(a1);
     System.out.println(arrayToString(b1));
     a1[3] = 7;
     System.out.println(arrayToString(a1));
     System.out.println(arrayToString(b1));
+
+    // finally, testing concatArray
+    int[] d1 = new int[] {1, 2, 3, 4, 5};
+    int[] d2 = new int[] {1, 4, 9, 16, 25};
+    int[] d3 = concatArray(d1, d2);
+    System.out.println(arrayToString(d1));
+    System.out.println(arrayToString(d2));
+    System.out.println(arrayToString(d3));
   }
   public static String arrayToString(int[] nums) {
     if (nums.length == 0) {return "[]";}
@@ -29,7 +39,7 @@ public class MyArrays {
   }
 
   public static int[] returnCopy(int[] arr) {
-    //
+    // returns a copy of the given array
     int[] c1 = new int[arr.length];
     for (int i = 0; i < arr.length; i++) {
       c1[i] = arr[i];
@@ -37,7 +47,14 @@ public class MyArrays {
     return c1;
   }
   public static int[] concatArray(int[]arr1, int[]arr2) {
-    //
-    return null;
+    //returns a new array containing all values from both arrays given in the same order as given
+    int[] arr3 = new int[arr1.length + arr2.length];
+    for (int i = 0; i < arr1.length; i++) {
+      arr3[i] = arr1[i];
+    }
+    for (int j = 0; j < arr2.length; j++) {
+      arr3[j + arr1.length] = arr2[j];
+    }
+    return arr3;
   }
 }
