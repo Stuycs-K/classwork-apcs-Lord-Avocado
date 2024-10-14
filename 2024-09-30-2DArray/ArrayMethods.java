89497604050e5c71eff6c7fd1cef4e717acfcc67
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-14 19:59:39 -0400

    initialized class, added names and emails in comment form, copied arrayToString(int[]) function

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,1 +1,24 @@
+//Ahana Chandra achandra60@stuy.edu
+//Arko Chakrabartiroy achakrabartiroy60@stuy.edu
 
+
+public class ArrayMethods {
+  //
+
+
+  public static String arrayToString(int[] nums) {
+    if (nums.length == 0) {return "[]";}
+    String value = "[";
+    for (int i = 0; i < nums.length; i++) {
+      value += nums[i];
+      if (i != nums.length-1) {
+        //i is not the last element in the array
+        value += ", ";
+      } else {
+        //i is the last element in the array
+        value += "]";
+      }
+    }
+    return value;
+  }
+}
