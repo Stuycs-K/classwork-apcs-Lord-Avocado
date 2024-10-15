e1a6289691c8c7a3f5429b691f3ac4540758d6f6
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-14 20:11:39 -0400

    created and tested arrayToString(int[][]) function

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -4,13 +4,36 @@
 
 public class ArrayMethods {
   //
-
+  public static void main(String[] args) {
+    //first testing arraytoString(int[][])
+    int[][] a1 = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println(arrayToString(a1));
+    int[][]  b1 = new int[][] {{1, 4, 9, 16}, {1, 8, 27}, {1, 16}, {1, 32, 243, 1024}};
+    System.out.println(arrayToString(b1));
+  }
 
   public static String arrayToString(int[] nums) {
     if (nums.length == 0) {return "[]";}
     String value = "[";
     for (int i = 0; i < nums.length; i++) {
       value += nums[i];
+      if (i != nums.length-1) {
+        //i is not the last element in the array
+        value += ", ";
+      } else {
+        //i is the last element in the array
+        value += "]";
+      }
+    }
+    return value;
+  }
+
+  // arrayToString, but with 2D Array parameter
+  public static String arrayToString(int[][] nums) {
+    //  use arrayToString(int[])
+    String value = "[";
+    for (int i = 0; i < nums.length; i++) {
+      value += arrayToString(nums[i]);
       if (i != nums.length-1) {
         //i is not the last element in the array
         value += ", ";
