c35051c782b38b4cf5473146db0b4d1411e9ee30
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-14 20:38:18 -0400

    Wrote and tested swapRC(int[][]) function, finalized code

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -14,6 +14,10 @@ public class ArrayMethods {
     // next, testing arr2DSum
     int[][] b1 = new int[][] {{1, 3, 5, 6, 9}, {3, 71, 69, 10}, {0, 1, 4, -100}}; // should return 82
     System.out.println(arr2DSum(b1));
+
+    // finally, testing swapRC
+    int[][] c1 = new int[][] {{1, 2, 3}, {4, 5, 6}};
+    System.out.println(arrayToString(swapRC(c1))); // should return {{1, 4}, {2, 5}, {3, 6}}
   }
 
   public static String arrayToString(int[] nums) {
@@ -59,6 +63,22 @@ public class ArrayMethods {
       for (int j = 0; j < nums[i].length; j++) {
         // columns
         value += nums[i][j];
+      }
+    }
+    return value;
+  }
+
+  // rotate array by switching rows and cols; you may assume the array is rectangular and neither rows or cols = 0
+  public static int[][] swapRC(int[][] nums) {
+    //
+    int rows = nums.length; // get the number of rows
+    int cols = nums[0].length; // get the number of columns; since array is rectangular nums[0].length = nums[1].length...etc
+    int[][] value = new int[cols][rows]; // make new array with rows and cols swapped
+    for (int i = 0; i < value.length; i++) {
+      //for rows of value, aka columns of nums
+      for (int j = 0; j < value[i].length; j++) {
+        //for columns of value, aka rows of nums
+        value[i][j] = nums[j][i];
       }
     }
     return value;
