2e4dc021d6bfe2e15c9cca5738e12e954912da41
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-15 09:57:37 -0400

    Initialized class and main, copied previous arrayToString() functions (both 1D and 2D array)

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -1,1 +1,44 @@
+import java.util.Arrays;
 
+public class ArrayDemo{
+  //
+  public static void main(String[] args) {
+    // tests go here
+    // now that java.util.Arrays is imported, Arrays.toString(array) can be used
+    // Arrays.deepToString(array) for 2D arrays
+  }
+
+  //arrToString method from before
+  public static String arrayToString(int[] nums) {
+    if (nums.length == 0) {return "[]";}
+    String value = "[";
+    for (int i = 0; i < nums.length; i++) {
+      value += nums[i];
+      if (i != nums.length-1) {
+        //i is not the last element in the array
+        value += ", ";
+      } else {
+        //i is the last element in the array
+        value += "]";
+      }
+    }
+    return value;
+  }
+
+  // 2D arrayToString
+  public static String arrayToString(int[][] nums) {
+    //  use arrayToString(int[])
+    String value = "[";
+    for (int i = 0; i < nums.length; i++) {
+      value += arrayToString(nums[i]);
+      if (i != nums.length-1) {
+        //i is not the last element in the array
+        value += ", ";
+      } else {
+        //i is the last element in the array
+        value += "]";
+      }
+    }
+    return value;
+  }
+}
