4e9a7470abd75e49738f4336eea1db42d1464c64
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-16 10:10:45 -0400

    Wrote countZeros2D() and arr2DSum() and tested

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -6,6 +6,14 @@ public class ArrayDemo{
     // tests go here
     // now that java.util.Arrays is imported, Arrays.toString(array) can be used
     // Arrays.deepToString(array) for 2D arrays
+    int[][] a1 = new int[][] {{1, 2, 3}, {8, 3, 5}, {2, 4, 6, 9}};
+    System.out.println(Arrays.deepToString(a1));
+    System.out.println(arrayToString(a1));
+    System.out.println("\n"); // line break to indicate next test
+    int[] b1 = new int[] {5, 23, 62, 17, 3025};
+    System.out.println(Arrays.toString(b1));
+    System.out.println(arrayToString(b1));
+    //
   }
 
   //arrToString method from before
@@ -40,5 +48,34 @@ public class ArrayDemo{
       }
     }
     return value;
+  }
+
+  // calculate and return the number of elements equal to 0 in the given 2D array
+  public static int countZeros2D(int[][] nums) {
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      // i is rows
+      for (int j = 0; j < nums[i].length; j++) {
+        // columns
+        if (nums[i][j] == 0) {
+          count += 1;
+        }
+      }
+    }
+    return count;
+  }
+
+
+  // calculate the sum of all values in a 2D array
+  public static int arr2DSum(int[][] nums) {
+    int total = 0;
+    for (int i = 0; i < nums.length; i++) {
+      // i is rows
+      for (int j = 0; j < nums[i].length; j++) {
+        // j is columns in a given row
+        total += nums[i][j];
+      }
+    }
+    return total;
   }
 }
