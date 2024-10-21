8360e6f256373b9a662ad47b7175fde169adf446
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-20 23:23:41 -0400

    wrote and tested distance and stringTo functions

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -1,1 +1,44 @@
+public class Point{
+  private double x,y;
 
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+
+  //Initialize this Point to have the same values as the other Point.
+  public Point(Point other){
+    //COMPLETE THIS SECOND
+    //Then write test cases.
+    this.x = other.getX();
+    this.y = other.getY();
+  }
+
+  //Initialize this Point to have the provided values
+  public Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  /*
+  Write this method FOURTH, go to the main and do that one first.
+  */
+  public double distanceTo(Point other){
+    return Math.sqrt((this.getX() - other.getX()) * (this.getX() - other.getX()) + (this.getY() - other.getY()) * (this.getY() - other.getY()));
+  }
+
+  /*
+  Write this method last. Re-use (NOT COPY/PASTE) prior work and
+  do not write redundant code.
+  */
+  public static double distance(Point a, Point b){
+    return a.distanceTo(b);
+  }
+
+
+}
