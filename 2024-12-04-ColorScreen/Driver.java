37aa8470665d644d9c5a1c5286ea2a8796723998
Lord-Avocado <arkoc@nycstudents.net>
2024-12-12 09:11:15 -0500

    Added Driver class with main method; initialized helper functions.

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
 
+public class Driver {
+  // 
+  public static void main(String[] args) {
+    // 1. create 80x80 border of color (can be multiple, including foreground and background) around the screen. Should be 1 character thick.
+    // 2. make an array of 3 random integers from 0-99 inclusive
+    // 3. display the three integers made, evenly spaced (approximately), on the row below the top border.
+    // 4. depending on the values of the integers, print them in the following colors:
+    // <25 = bright red, >75 = bright green, otherwise default to white (assuming terminal color is black).
+    // 5. On the line below the random numbers, draw a horizontal seperator (can match border color but use a different character).
+    // 6. (DO THIS AFTER EVERYTHING ELSE) move cursor to line 31 before program exits; otherwise, your terminal will draw over your screen.
+    // 7. Extra: draw something in the middle of your screen (before you move your cursor to the end).
+    // 
+  }
+  public static void createBorder(int x, int y, String fcolor, String bcolor, int thickness) {
+    // 
+  }
+  public static ArrayList<Integer> generateArray(int length, int range, int shift) {
+    // first make a new ArrayList<Integer>
+    return;
+  }
+  public static void drawHorizontal(char a, String fcolor, String bcolor) {
+    // 
+  }
+  public static void endProgram() {
+    // 
+  }
+}
